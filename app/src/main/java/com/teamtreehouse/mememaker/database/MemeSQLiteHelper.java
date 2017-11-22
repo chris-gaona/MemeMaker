dc1ed1accf73caa42e0d49efc8a9600b3d51ebb3
Chris Gaona <thebeloved88@sbcglobal.net>
2017-11-22 09:01:26 -0800

    Adds the annotations table

--- app/src/main/java/com/teamtreehouse/mememaker/database/MemeSQLiteHelper.java
+++ app/src/main/java/com/teamtreehouse/mememaker/database/MemeSQLiteHelper.java
@@ -10,7 +10,7 @@ public class MemeSQLiteHelper extends SQLiteOpenHelper {
     private static final String DB_NAME = "memes.db";
     private static final int DB_VERSION = 1;
 
-    //Meme Table functionality
+    // Meme Table functionality
     public static final String MEMES_TABLE = "MEMES";
     public static final String COLUMN_MEME_ASSET = "ASSET";
     public static final String COLUMN_MEME_NAME = "NAME";
@@ -20,7 +20,22 @@ public class MemeSQLiteHelper extends SQLiteOpenHelper {
             COLUMN_MEME_ASSET + " TEXT," +
             COLUMN_MEME_NAME + " TEXT)";
 
-    //Meme Table Annotations functionality
+    // Meme Table Annotations functionality
+    public static final String ANNOTATIONS_TABLE = "ANNOTATIONS";
+    public static final String COLUMN_ANNOTATION_COLOR = "COLOR";
+    public static final String COLUMN_ANNOTATION_X = "X";
+    public static final String COLUMN_ANNOTATION_Y = "Y";
+    public static final String COLUMN_ANNOTATION_TITLE = "TITLE";
+    public static final String COLUMN_FOREIGN_KEY_MEME = "MEME_ID";
+
+    private static final String CREATE_ANNOTATIONS = "CREATE TABLE " + ANNOTATIONS_TABLE + " (" +
+            BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+            COLUMN_ANNOTATION_X + " INTEGER, " +
+            COLUMN_ANNOTATION_Y + " INTEGER, " +
+            COLUMN_ANNOTATION_TITLE + " TEXT, " +
+            COLUMN_ANNOTATION_COLOR + " TEXT, " +
+            COLUMN_FOREIGN_KEY_MEME + " INTEGER, " +
+            "FOREIGN KEY(" + COLUMN_FOREIGN_KEY_MEME + ") REFERENCES MEMES(_ID))";
 
     public MemeSQLiteHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -29,6 +44,7 @@ public class MemeSQLiteHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL(CREATE_MEMES);
+        sqLiteDatabase.execSQL(CREATE_ANNOTATIONS);
     }
 
     @Override
